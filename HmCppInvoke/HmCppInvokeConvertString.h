63553ec6828e2aad774304ba47d5a83413cb17f5
komiyamma <komiyama0629@gmail.com>
2021-07-12 12:28:03 +0900

    正確には932ではなくOEMという旨

--- HmCppInvoke/HmCppInvokeConvertString.h
+++ HmCppInvoke/HmCppInvokeConvertString.h
@@ -17,10 +17,10 @@ namespace Hidemaru {
 			// utf16→utf8へ
 			static std::string utf16_to_utf8(const std::wstring& text);
 
-			// cp932→utf16へ
+			// cp932→utf16へ(正確には932ではなくシステムOEMコードページ。Windows日本語モードだと932)
 			static std::wstring cp932_to_utf16(const std::string& text);
 
-			// utf16→cp932へ
+			// utf16→cp932へ(正確には932ではなくシステムOEMコードページ。Windows日本語モードだと932)
 			static std::string utf16_to_cp932(const std::wstring& text);
 		};
 	}
