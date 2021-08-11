12480312475bf2df5441a766625bc9eeb8fcf445
komiyamma <komiyama0629@gmail.com>
2021-08-11 12:04:11 +0900

    HmCppInvokeConvertString はインクルードしてしまうことにした。

--- HmCppInvoke/HmCppInvoke.h
+++ HmCppInvoke/HmCppInvoke.h
@@ -1,6 +1,6 @@
 #pragma once
 /*
- * Hm.CppInvoke ver 2.041
+ * Hm.CppInvoke ver 2.042
  * Copyright (c) 2021 Akitsugu Komiyama
  * under the MIT License
  */
@@ -17,6 +17,7 @@
 #include <set>
 #include <optional>
 
+#include "HmCppInvokeConvertString.h"
 
 namespace Hidemaru {
     /// <summary>
