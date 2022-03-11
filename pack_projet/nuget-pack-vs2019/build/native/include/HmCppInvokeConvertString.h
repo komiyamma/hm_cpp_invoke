74b9af7b94f8c42ef03d0413801c54961be8d5df
komiyamma <komiyama0629@gmail.com>
2022-03-11 09:39:07 +0900

    vs2019用も追加

---
+++ pack_projet/nuget-pack-vs2019/build/native/include/HmCppInvokeConvertString.h
@@ -0,0 +1,40 @@
+#pragma once
+/*
+ * Copyright (c) 2021-2022 Akitsugu Komiyama
+ * under the MIT License
+ */
+
+#include <string>
+
+namespace Hidemaru {
+	namespace Text {
+
+		/// <summary>
+		/// よくある文字エンコード変換系
+		/// </summary>
+		class Encoding {
+		public:
+			/// <summary>
+			/// 「string型のutf8」を「wstring型のutf16」へ
+			/// </summary>
+			static std::wstring utf8_to_utf16(const std::string& text);
+
+			/// <summary>
+			/// 「wstring型のutf16」を「string型のutf8」へ
+			/// </summary>
+			static std::string utf16_to_utf8(const std::wstring& text);
+
+			/// <summary>
+			/// 「string型のcp932」を「wstring型のutf16」へ
+			/// <para>(正確には932ではなくシステムOEMコードページ。Windows日本語モードだと932)</para>
+			/// </summary>
+			static std::wstring cp932_to_utf16(const std::string& text);
+
+			/// <summary>
+			/// 「wstring型のutf16」を「string型のcp932」へ
+			/// <para>(正確には932ではなくシステムOEMコードページ。Windows日本語モードだと932)</para>
+			/// </summary>
+			static std::string utf16_to_cp932(const std::wstring& text);
+		};
+	}
+}
